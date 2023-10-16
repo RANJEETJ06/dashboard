f9f456c60d15e7787bd0a0769b37d3fff2534552
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2023-10-16 23:25:34 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}></Route>
+            <Route path="login" element={<Login/>}></Route>
+            <Route path="users">
+              <Route index element={<List/>}></Route>
+              <Route path=":userId" element={<Single/>}></Route>
+              <Route path="new" element={<New/>}></Route>
+            </Route>
+            <Route path="product">
+              <Route index element={<List/>}></Route>
+              <Route path=":productId" element={<Single/>}></Route>
+              <Route path="new" element={<New/>}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
