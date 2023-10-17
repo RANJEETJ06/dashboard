c38e733fc3412b766d05448bdda3a79df53ea9e0
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2023-10-17 21:21:34 +0530

    list

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,31 @@
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns,userRows } from "../../datatablesource";
+
+const Datatable = () => {
+    const actionColumn =[{field:"action",headerName:"Action",width:200,renderCell:()=>{
+        return(
+            <div className="cellAction">
+                <div className="view">View</div>
+                <div className="delete">Delete</div>
+            </div>
+        );
+    }}]
+  return (
+    <div className="datatable">
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,16 @@
+import Datatable from "../../components/datatable/Datatable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./list.scss";
 
 const List = () => {
   return (
     <div className='list'>
-      <h1>list</h1>
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
     </div>
   )
 }
